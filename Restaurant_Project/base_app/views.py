a360b61a347681fba57977d613357b1d23100ec4
salonipatell <120467387+salonipatell@users.noreply.github.com>
2024-10-01 22:49:54 +0530

    first commit with basic django settings

---
+++ Restaurant_Project/base_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def HomeView(request):
+    pass
+
+def AboutView(request):
+    pass
+
+
+def MenuView(request):
+    pass
+
+
+def BookTableView(request):
+    pass
